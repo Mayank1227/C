647ef394c30f19f2b865cdc30a98fc0724ae2a0b
Mayank1227 <136098348+Mayank1227@users.noreply.github.com>
2023-11-01 13:39:07 +0530

    Add files via upload

---
+++ divisibility of a number 5 and 11.c
@@ -0,0 +1,16 @@
+int main()
+{
+	int num;
+	printf("enter any numbers: ");
+	scanf("%d",&num);
+	if((num%5==0)&&(num%11==0))
+	{
+		printf("num is divisible by 5 and 11");
+	}
+	else
+	{
+		printf("num is not divisible by 5 and 11");
+	}
+	return 0;
+}
+
